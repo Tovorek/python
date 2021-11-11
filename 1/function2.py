7a943f7c17172317c5e0f6652a4eea21cdce21f7
Aaromal Sujith <aaromalsujith01@gmail.com>
2021-11-11 16:13:04 +0530

    commitbeforeIstart

--- 1/function2.py
+++ 1/function2.py
@@ -1,6 +1,6 @@
-# function with an input
+# Print square of a number
+x = input("Enter the digit==")
+def square( x ):
+	return x * x 
+print (x)
 
-def greet( name ):
-    print("Hi"+ name + "!!")
-
-greet(" Aaromal Sujith ")    
