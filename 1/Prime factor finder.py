487144f8b5b97d054964b3fbe052250069799f11
buhuhaha <aaromalsujith01@gmail.com>
2021-11-13 12:23:25 +0530

    12

---
+++ 1/Prime factor finder.py
@@ -0,0 +1,13 @@
+num = input ("Enter number :")
+print(num)
+
+def get_prime_factors(num):
+    factors = list()
+    divisor = 2
+    while(divisor <= num):
+         if (num % divisor) == 0:
+            factors.append(divisor)
+            num = num/divisor
+         else:
+            divisor +=1
+    return factors        
